e72611c8fd0ea4ff8919e2ad7b9d33d36044e9ca
David Larsen <david.larsen@gmail.com>
2012-03-28 10:05:22 -0700

    Initial commit of JS

---
+++ jquery.sumtr.js
@@ -0,0 +1,51 @@
+;(function($) {
+
+  $.fn.sumtr = function(options) {
+
+    var settings = null;
+    if(typeof option === 'object')
+    {
+        settings = option;
+    }
+
+    // extend user settings with default settings
+    settings = $.extend({}, $.fn.sumtr.defaultSettings, settings || {});
+
+    this.each(function() {
+      var s = []; // we hold the results here
+
+      var table       = $(this);
+      var bodyRows    = $(this).find(settings.bodyRows);
+      var summaryRows = $(this).find(settings.summaryRows);
+
+      bodyRows.each(function(index) {
+          var col = 0;
+          $(this).children("td").each(function() {
+              if ($(this).is(settings.sumCells)){
+                if (s.length < col + 1) s[col] = 0;
+                var val = settings.readValue($(this));
+                s[col] = s[col] + val;
+              } else {
+                s[col] = "noCount"; // a flag which tells us we're not counting that column
+              }
+              col++;
+          });
+      });
+
+      summaryRows.each(function(index) {
+        var col = 0;
+        $(this).children("td").each(function() {
+            if (s[++col] != "noCount") $(this).html(s[col]);
+        });
+      });
+    });
+  };
+
+  $.fn.sumtr.defaultSettings = {
+    readValue : function(e) { return parseFloat(e.html()); },
+    sumCells : '.sum',
+    bodyRows : 'tbody tr',
+    summaryRows : 'tr.summary',
+  };
+
+})(jQuery);
